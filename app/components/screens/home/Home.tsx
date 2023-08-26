42bf0d26e4a3fcd00a07d8a0dbcb785e6bb83a1f
Irina Litvinova <neforsha@yandex.ru>
2023-08-26 15:53:09 +0300

    fix: refactoring

--- app/components/screens/home/Home.tsx
+++ app/components/screens/home/Home.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import {Pressable, SafeAreaView, Text} from "react-native";
-import {useTypedNavigation} from "@/hooks/useTypedNavigation";
+import { Pressable, SafeAreaView, Text } from 'react-native';
+import { useTypedNavigation } from '@/hooks/useTypedNavigation';
 
 const Home = () => {
-    const {navigate} = useTypedNavigation()
-    return (
-        <SafeAreaView>
-            <Text>Home</Text>
-            <Pressable onPress={() => navigate('Auth')}>
-                <Text style={{color: 'white', marginTop: 60}}>Go to login</Text>
-            </Pressable>
-        </SafeAreaView>
-    );
+	const { navigate } = useTypedNavigation();
+	return (
+		<SafeAreaView>
+			<Text>Home</Text>
+			<Pressable onPress={() => navigate('Auth')}>
+				<Text style={{ color: 'white', marginTop: 60 }}>Go to login</Text>
+			</Pressable>
+		</SafeAreaView>
+	);
 };
 
 export default Home;
